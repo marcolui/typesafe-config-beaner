461cb922bb254cded2f356f7048a3f1dc19e1916
marco <lui.marco@gmail.com>
2017-12-03 20:46:52 +0800

    removed unwanted files

--- src/main/java/mago/config/ConfigException.java
+++
@@ -1,9 +0,0 @@
-package mago.config;
-
-public class ConfigException extends RuntimeException {
-    public ConfigException(String message) { super(message); }
-
-    public ConfigException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/mago/config/ConfigNameResolver.java
+++
@@ -1,5 +0,0 @@
-package mago.config;
-
-public interface ConfigNameResolver {
-    String resolve();
-}
